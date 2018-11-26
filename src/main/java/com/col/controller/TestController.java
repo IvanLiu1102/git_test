9c9c54705469c15f966d95678ae2452846e3645b
led_min@126.com <led-min1102>
2018-11-26 11:46:33 +0800

    1

--- src/main/java/com/col/controller/TestController.java
+++ src/main/java/com/col/controller/TestController.java
@@ -7,5 +7,6 @@ public class TestController {
 
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaaaaaa");
+        System.out.println("bbbbbbbbbbbbbbbbb");
     }
 }
