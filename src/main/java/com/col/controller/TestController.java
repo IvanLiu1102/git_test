e312b9d56e8730858290eab70fbf430bb5d2f3a3
led_min@126.com <led-min1102>
2018-11-26 11:07:28 +0800

    1

---
+++ src/main/java/com/col/controller/TestController.java
@@ -0,0 +1,11 @@
+package com.col.controller;
+
+/**
+ * Created by liu.min on 2018/11/26.
+ */
+public class TestController {
+
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaaaaaaa");
+    }
+}
