63a9df52c92478b4f282aa25657292121d89d563
led_min@126.com <led-min1102>
2018-11-26 11:54:23 +0800

    1

--- src/main/java/com/col/controller/TestController.java
+++ src/main/java/com/col/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.col.controller;
 
+import org.apache.logging.log4j.util.StackLocatorUtil;
+
 /**
  * Created by liu.min on 2018/11/26.
  */
@@ -8,5 +10,6 @@ public class TestController {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaaaaaa");
         System.out.println("bbbbbbbbbbbbbbbbb");
+        System.out.println("ccccccccccccccccc");
     }
 }
